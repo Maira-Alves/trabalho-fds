78d754b36e9086b7f55df6e10cc80a2bd1e753d4
Maira-Alves <131826252+Maira-Alves@users.noreply.github.com>
2025-06-26 14:09:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function cliqueGarota() {
+  const botao = document.getElementById("botaoGarota");
+  const msg = document.getElementById("mensagem");
+
+  botao.innerText = "Muito obrigada por ter lido até aqui. Espero que tenha gostado! ♥️";
+  botao.style.backgroundColor = "#ffcce0";
+}
+
+
+ 
